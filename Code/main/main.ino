93e68cbd847e050271d0ac862464fccdf6411a39
keilev26 <keilevcs4@gmail.com>
2025-01-23 23:37:28 +0100

    sending

--- Code/main/main.ino
+++ Code/main/main.ino
@@ -1,35 +1,49 @@
 #include <QTRSensors.h>
 #include "motors.h"
-
+#include <VL53L0X.h> //Include the VL53L0X library
+#include "Wire.h" //The Wire.h library is required for I2C communication
 
 //variables
-int max_vel=250;
+int max_vel=180;
 int error=0;
 int last_time=0;
 int seconds;
 int last_error=0;
 int UMBRAL_WHITE=30;
+int umbral_dist=40;
 int time_for_task_1=(60)*1000;//60 seconds
-float kp=0.16;
-float ki=0.003;
-float kd=0.6;
-float vel;
 
+
+
+int distance;
+int last_distance;
+int diff=0;
+int right_vel=50;
+int left_vel=-50;
+
+
+
+float kp=0.025;
+float ki=0;
+float kd=0;
+float vel=0;
+bool read=true;
 
 //pines del motor      
-#define NUM_SENSORS 5
+#define NUM_SENSORS 8
 #define TIMEOUT 2500
-#define EMITTER_PIN 2 
-const int pinAIN2=7;
-const int pinAIN1=8;
-const int pinBIN1=9;
-const int pinBIN2=10;
+#define EMITTER_PIN 4 
+const int pinAIN2=10;
+const int pinAIN1=9;
+const int pinBIN1=5;
+const int pinBIN2=6;
 const int pinSTBY=12;
 
 
 //QTR DECLARATION
 QTRSensors qtr;
-unsigned int sensorValues[NUM_SENSORS];
+const uint8_t SensorCount = 8;
+uint16_t sensorValues[SensorCount];
 unsigned int position=0;
 
 
@@ -44,35 +58,66 @@ enum states{
 };
 states task=doing_task_1;
 
+VL53L0X L0X1 = VL53L0X();
+
 
 
 void setup() {
   // INITIALIZE QTR SENSORS
-  qtr.setTypeRC(); 
-  qtr.setSensorPins((const uint8_t[]){14,15,16,17,18,19,20,21}, 8);
   
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){14,15,16,17,18,19,20,21},8);//{14,15,16,17,18,19,20,21},8;
+  qtr.setEmitterPin(EMITTER_PIN);
+  //Wire.begin();
   // CALIBRATION
   delay(500);
+
+  //if(!L0X1.init()){ //Initialize sensor 1 and wait for answer
+  //                  //If the answer is false, then send an error
+  //                  //message and wait for restart
+  //  Serial.println("Sensor error");
+  //  while(1); //Arduino enters infinite loop
+  //}
+
+  //L0X1.setTimeout(500);
+  //L0X1.setMeasurementTimingBudget(200000); //Every 200ms a measurement is possible
+  //L0X1.startContinuous();
+  Serial.begin(9600);
+  Serial.println("CALIBRATION");
   for(int i=0;i<400;i++){
     qtr.calibrate();
   }
+  Serial.println("CALIBRATION DONE");
+  //two_motors.setup();
   delay(1000);
-  two_motors.setup();
 }
 
 void loop() {
   switch(task){
         //TASK1//
         case doing_task_1:{
+          two_motors.avanzar_after_PID(255,255);//CHANGING OUR MOTORS VELOCITY
           
           position=qtr.readLineBlack(sensorValues);//READ LINE
+          for (uint8_t i = 0; i < 8; i++){
+            Serial.print(sensorValues[i]);
+            Serial.print('\t');
+          }
+          
+          
+          Serial.print(position);
+          Serial.print('\t');
           error=((position)-(3500));//CALCULATIN THE ERROR RESPECTING THE CENTER VALUE
           seconds=(millis()-last_time);//GAP OF TIME
           vel=two_motors.PID(error,seconds,last_error,ki,kp,kd,max_vel);//USING PID FOR CALCULATIN THE VARIATION OF VELOCITY OUR MOTORS NEED
-          two_motors.avanzar_after_PID(max_vel-vel,max_vel+vel);//CHANGING OUR MOTORS VELOCITY 
+          Serial.print('\t');
+          Serial.println(vel);
+          two_motors.avanzar_after_PID(max_vel-vel,max_vel+vel);//CHANGING OUR MOTORS VELOCITY
           last_error=error;
           last_time=millis();
-          //
+          delay(100);
+          
+          /*
           bool change_state=true;
           for(int i=0;i<8;i++){
              if(sensorValues[i]>UMBRAL_WHITE){
@@ -83,12 +128,34 @@ void loop() {
           if(millis()>time_for_task_1 || change_state){
             task=doing_task_2;
           }
+          */
           break;
         }
 
 
         case doing_task_2:{
+          Serial.println("in task 2");
+          //two_motors.avanzar_after_PID(left_vel,right_vel);
+          //Serial.print("Sensor[mm]: ");
+          //distance=L0X1.readRangeContinuousMillimeters();
+          //Serial.println(distance);
+          ////Uncomment the code below for single measurement
+          ////Serial.println(L0X1.readRangeSingleMillimeters());
+          //if (L0X1.timeoutOccurred()) { Serial.println(" TIMEOUT"); } 
+
+
+          //if(read){
+          //   last_distance=distance;
+          //   read=false;
+          //}
+          //else{
+          //  diff=distance-last_distance;
+          //  if(diff<umbral_dist){
+          //    right_Vel=100;left_vel=100;
+          //  }
+          //}
           
+          break;
         }
     
   }
--- Code/main/motors.ino
+++ Code/main/motors.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "motors.h"
 Motors::Motors(int pinAIN1, int pinAIN2, int pinBIN1, int pinBIN2,int pinSTBY){
-   this->pinSTBY=pinSTBY;
+   //this->pinSTBY=pinSTBY;
    this->pinMotorA[0]=pinAIN1;
    this->pinMotorA[1]=pinAIN2;
    this->pinMotorB[0]=pinBIN1;
@@ -12,32 +12,48 @@ void Motors::setup(){
   pinMode(pinMotorA[1], OUTPUT);
   pinMode(pinMotorB[0], OUTPUT);
   pinMode(pinMotorB[1], OUTPUT);
-  digitalWrite(pinSTBY,HIGH);
 }
 void Motors::avanzar_after_PID(int speedA, int speedB){
   if(speedA>0){
     analogWrite(pinMotorA[1], speedA);
-    analogWrite(pinMotorA[2], 0);
+    digitalWrite(pinMotorA[2], LOW);
   }else{
-    analogWrite(pinMotorA[1], 0);
-    analogWrite(pinMotorA[2], speedA);
+    digitalWrite(pinMotorA[1], LOW);
+    analogWrite(pinMotorA[2], -speedA);
   }
   if(speedB>0){
-    digitalWrite(pinMotorB[1], speedB);
-    digitalWrite(pinMotorB[2], 0);
+    analogWrite(pinMotorB[1], speedB);
+    digitalWrite(pinMotorB[2], LOW);
   }else{
-    digitalWrite(pinMotorB[1], 0);
-    digitalWrite(pinMotorB[2], speedB);
+    digitalWrite(pinMotorB[1], LOW);
+    analogWrite(pinMotorB[2], -speedB);
   }
 }
 float Motors::PID(int error, int seconds,int last_error,float ki,float kp,float kd,int max_vel){
-  if(error<=2000 && error>=-2000 ){
-    float derivative_error= (error-last_error)/seconds;
-    float integral_error=(error+last_error)*seconds/2.0;
-    float proportional_error=error*1.0;
-    float vel=(int)(kp*proportional_error+ki*integral_error+kd*derivative_error);
+  float vel;
+  if(error<=3500 && error>=-3500 ){
+    float derivative_error= 1.0*(error-last_error)/seconds;
+    float integral_error=1.0*(error+last_error)*seconds/2.0;
+    float proportional_error=1.0*error;
+    
+    Serial.print(seconds);
+    Serial.print('\t');
+    Serial.print('\t');
+    Serial.print(derivative_error);
+    Serial.print('\t');
+    Serial.print(integral_error);
+    Serial.print('\t');
+    Serial.print(proportional_error);
+    Serial.print('\t');
+    Serial.print('\t');
+    vel=(1.0*kp*proportional_error+1.0*ki*integral_error+1.0*kd*derivative_error);
+    Serial.print(vel);
+    Serial.print('\t');
     if(vel>max_vel){vel=max_vel;}
     if(vel<-max_vel){vel=-max_vel;}
+
+
+    
   }
   else{
     vel=-10000;
